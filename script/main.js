8f1f31b13efa4e67e19131d1c3aa7e84f83973ce
Marakuya90 <zhilzhil@mail.ru>
2023-02-27 15:47:51 +0300

    dropdown menu

--- script/main.js
+++ script/main.js
@@ -1,24 +1,33 @@
+let intervalId;
 
-document.querySelectorAll('.module_btn')
-.forEach(el=>addEventListener('click', el => { 
-    const menu = el.currentTarget;
-}));
+document.querySelectorAll('.toggle_btn')
+    .forEach(e => {
+        e.addEventListener('click', e => {
+            const menu = e.currentTarget.dataset.path;
+            document.querySelectorAll('.dropdown-menu')
+            .forEach( e => {
+                if(!document.querySelector(`[data-target=${menu}]`)
+                .classList.contains('open')){
+                    e.classList.remove('menu-active');
+                    e.classList.remove('open');
+                    document.querySelector(`[data-target=${menu}]`)
+                    .classList.add('menu-active');
+                    intervalId = setTimeout(() => {
+                        document.querySelector(`[data-target=${menu}]`)
+                        .classList.add('open');
+                    },0);
+                }
+                if (document.querySelector(`[data-target=${menu}]`)
+                .classList.contains('open')) {
+                    clearTimeout(intervalId);
+                    document.querySelector(`[data-target=${menu}]`)
+                    .classList.remove('menu-active');
+                    intervalId = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`)
+                    .classList.remove('open');
+                },0);
+                }
+            });
+        });
+    });
 
-function expandmodule(e) {
-    if (check == 0) {
-    e.stopPropagation();
-    let div = document.createElement('div');
-    div.innerHTML =  '<ul><li>Тема 1. Куда движутся IT тренды? Аналитика Gartner, разбор отчетов консалтинговых компаний<li/><li>Тема 2. Рынки Ближнего Востока, Азии, Латинской Америки<li/><li>Тема 3. Что такое внутренние и внешние инновации? Как искать инновационные идеи?<li/><li>9 видео роликов, вебинар с приглашенным экспертом<li/></ul>';
-    div.classList.add('module_past');
-    console.log(e.target.parentNode);
-    e.target.parentNode.style.margin = '0px';
-    e.target.parentNode.style.border = '1px solid #E9E9E9';
-
-    document.querySelector('.module').after(div);
-    check++;
-    console.log(check);
-}
-else {
-    console.log(check);
-}
- }
